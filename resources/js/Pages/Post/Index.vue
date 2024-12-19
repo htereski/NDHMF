05e6eb91002d199dc7d9f04adc1b04bfb98a3286
htereski <tereski84@gmail.com>
2024-12-19 16:56:17 -0300

    style(post): ajustando alguns estilos

--- resources/js/Pages/Post/Index.vue
+++ resources/js/Pages/Post/Index.vue
@@ -1,7 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue'
 import PrimaryButton from '@/Components/PrimaryButton.vue'
-import BoldIcon from 'vue-material-design-icons/FormatBold.vue'
 import { Link } from '@inertiajs/vue3'
 
 const props = defineProps({
@@ -9,40 +8,49 @@ const props = defineProps({
   user: Object,
 })
 </script>
-<template>
-  <AppLayout
-    title="Núcleo de Direitos Humanos Marielle Franco - Postagens"
-    :isAuthenticated="user"
-    :user="user"
-  >
-    <div class="container mx-auto my-8 bg-gray-200 shadow-lg rounded-lg p-6">
-      <h2 class="text-2xl text-black mb-5">Postagens</h2>
 
+<template>
+  <AppLayout title="Postagens" :isAuthenticated="user" :user="user">
+    <div
+      class="bg-green-400 min-w-full h-[200px] flex justify-center items-center text-center"
+    >
+      <h2 class="text-2xl text-white mb-5">
+        Fique por dentro das últimas postagens
+      </h2>
+    </div>
+
+    <div
+      class="container mx-auto bg-gray-200 shadow-md p-6 flex flex-col items-start"
+    >
       <template v-if="user">
         <Link v-if="user.role.name != 'vitima'" :href="route('post.create')">
           <PrimaryButton class="mb-4">Criar postagem</PrimaryButton>
         </Link>
       </template>
 
-      <div class="flex flex-wrap justify-center gap-6">
+      <div class="flex flex-wrap justify-start gap-6">
         <template v-for="post in posts" :key="post.id">
           <Link :href="route('post.show', post)">
-            <div class="card w-80 h-96 shadow-xl rounded-3xl bg-white">
-              <figure class="h-2/3">
+            <div class="card w-80 h-96 shadow-xl rounded-xl bg-green-400">
+              <figure class="h-2/3 p-2">
                 <span
-                  class="absolute inset-0 flex items-start justify-end pr-5 pt-2 text-black"
+                  class="absolute inset-0 flex items-start justify-end text-black"
                 >
                 </span>
                 <img
                   :src="post.imagem"
                   alt="foto"
-                  class="w-full h-full object-cover"
+                  class="w-full h-full object-cover rounded bg-white"
                 />
               </figure>
               <div class="card-body h-1/3">
-                <h2 class="card-title h-full text-black mx-auto">
+                <h2 class="card-title text-black mx-auto">
                   {{ post.titulo }}
                 </h2>
+                <div
+                  class="truncate prose text-black"
+                  v-html="post.texto"
+                ></div>
               </div>
             </div>
           </Link>
--- resources/js/Pages/Post/Show.vue
+++ resources/js/Pages/Post/Show.vue
@@ -41,25 +41,26 @@ function deletePost() {
           {{ post.titulo }}
         </h2>
 
-        <span>Criado:</span>
-        <p class="prose-md">
+        <span class="text-black px-2 py-1 bg-slate-300 rounded">Criado:</span>
+        <p class="prose-md mt-1 mb-3">
           {{ formatDate(post.created_at) }} por {{ post.user.name }}
         </p>
 
         <template v-if="post.editors && post.editors.length > 0">
-          <span class="mt-3 flex">Edições:</span>
+          <span class="mt-3 text-black px-2 py-1 bg-slate-300 rounded"
+            >Edições:</span
+          >
           <template v-for="editors in post.editors" :key="editors.id">
-            <p class="prose-md">
+            <p class="prose-md mt-1">
               {{ formatDate(editors.pivot.created_at) }} por {{ editors.name }}
             </p>
           </template>
         </template>
 
         <div
-          class="my-8 prose-lg text-justify text-black w-full m-0 p-0"
+          class="my-8 prose-lg text-justify text-black w-full indent-10"
           v-html="post.texto"
         ></div>
-        <div></div>
       </div>
       <div
         class="my-8 bg-white flex flex-col gap-2 rounded-r-lg p-6 w-fit h-fit"
